58d30882b7725f761c5553dd924b72c98dc9413e
Raouda Njimogna <njimogna@unc.edu.com>
2024-08-26 17:55:10 -0400

    Finished Exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,9 @@
+"""My first exercise in COMP110"""
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
+
+
+__author__ = "730670839"
